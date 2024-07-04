ae9bb8939e56ae8271ccf4a40e3de7fcffe2dce0
choisungheeee <128767650+choisungheeee@users.noreply.github.com>
2024-07-04 13:54:34 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17972 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		
+		double a = stdin.nextDouble();
+		double b = stdin.nextDouble();
+		
+		System.out.print(a/b);
+	}
+
+}
