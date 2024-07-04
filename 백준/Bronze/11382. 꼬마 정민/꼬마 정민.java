dbf6b735839d753a7d34097831cf5e90c607442b
choisungheeee <128767650+choisungheeee@users.noreply.github.com>
2024-07-04 14:40:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		
+		long a = stdin.nextLong();
+		long b = stdin.nextLong();
+		long c = stdin.nextLong();
+		
+		System.out.print(a+b+c);
+	}
+}
