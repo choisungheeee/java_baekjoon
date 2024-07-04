8cd9ff17b2b688fdbda7c32991c1973b3a611fa8
choisungheeee <128767650+choisungheeee@users.noreply.github.com>
2024-07-04 14:00:11 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		
+		String str = stdin.next();
+			
+		System.out.print(str + "??!");
+	}
+
+}
