3ea3d18d38819c35203878d6510b33bc139aba89
choisungheeee <128767650+choisungheeee@users.noreply.github.com>
2024-07-05 15:16:54 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17920 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+
+		while(stdin.hasNext()) {
+			int a = stdin.nextInt();
+			int b = stdin.nextInt();
+			
+			System.out.println(a+b);
+		}
+	}
+}
